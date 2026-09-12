cf55f6c2c0d866e7d88bc5edcefbc07c0a06f017
AntonietaA <ae.aulestiar@uea.edu.ec>
2026-09-11 23:50:54 -0500

    Add files via upload

---
+++ #Calcular notas tarea 13.py
@@ -0,0 +1,9 @@
+#Calcular notas
+
+def calcular_promedio_notas(nota_1,nota_2):
+   return (nota_1 + nota_2)/2
+
+if __name__ == "__main__":
+    print("Promedio", calcular_promedio_notas(8,10))
+    print()
+
